6d5136ef941dc594135d69af01d5b1d3ae7c3bb2
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-27 11:44:37 -0400

    Fixed an error inside of my tailwind config file that was causing the text to be really light

--- googlesearchclone/src/components/navbar/NavBar.js
+++ googlesearchclone/src/components/navbar/NavBar.js
@@ -5,6 +5,11 @@ const NavBar = () => {
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
       <h1>NavBar</h1>
       <div className="flex justify-between items-center space-x-5 w-screen">
+        <NavLink to='/'>
+          <p className='text-2xl'>
+            Googly 🔎
+          </p>
+        </NavLink>
       </div>
     </div>
   )

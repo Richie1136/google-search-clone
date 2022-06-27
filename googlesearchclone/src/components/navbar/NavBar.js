ae26fe3b034d599c4c9e8b8a7e41e0d9e8d7124f
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-27 11:36:57 -0400

    Installed pacakges

--- googlesearchclone/src/components/navbar/NavBar.js
+++ googlesearchclone/src/components/navbar/NavBar.js
@@ -1,8 +1,11 @@
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
       <h1>NavBar</h1>
+      <div className="flex justify-between items-center space-x-5 w-screen">
+      </div>
     </div>
   )
 }

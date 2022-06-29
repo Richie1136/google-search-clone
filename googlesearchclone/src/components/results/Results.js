08f93f96d532fb1f5af8ad15281514dfdb3a8368
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-29 11:10:55 -0400

    Imported react packages from react, react-router-dom and react-player and using the context i just created inside of the Results component and descturing out the results, isLoading, getResults and searchTerm out of the useResultsContext hook

--- googlesearchclone/src/components/results/Results.js
+++ googlesearchclone/src/components/results/Results.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import ReactPlayer from 'react-player'
+
+import { useResultContext } from '../contextx/ResultContextProvider'
+
 
 const Results = () => {
+
+  const { results, isLoading, getResults, searchTerm } = useResultContext
   return (
     <div>Results</div>
   )

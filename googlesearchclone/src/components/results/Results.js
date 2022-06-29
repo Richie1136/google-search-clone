f7942541159b922e95d6828fa0af11caa650ab11
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-29 11:16:13 -0400

    Created a loading component

--- googlesearchclone/src/components/results/Results.js
+++ googlesearchclone/src/components/results/Results.js
@@ -3,11 +3,18 @@ import { useLocation } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 
 import { useResultContext } from '../contextx/ResultContextProvider'
+import Loading from '../loading/Loading'
 
 
 const Results = () => {
 
-  const { results, isLoading, getResults, searchTerm } = useResultContext
+  const { results, isLoading, getResults, searchTerm } = useResultContext()
+
+  const location = useLocation() // images, news, videos
+
+  if (isLoading) {
+    return <Loading />
+  }
   return (
     <div>Results</div>
   )

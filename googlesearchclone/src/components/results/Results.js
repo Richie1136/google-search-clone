1c44f8e51e64db3417461b71ba78dd9d6e2b5b20
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-30 11:06:16 -0400

    Moved my context folder out of the components folder and had to import my context inside of the index.js file and wrap the browserRouter and the app component inside of it

--- googlesearchclone/src/components/results/Results.js
+++ googlesearchclone/src/components/results/Results.js
@@ -2,22 +2,31 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import ReactPlayer from 'react-player'
 
-import { useResultContext } from '../contextx/ResultContextProvider'
+import { useResultContext } from '../../context/ResultContextProvider'
 import Loading from '../loading/Loading'
 
 
 const Results = () => {
 
-  // const { results, isLoading, getResults, searchTerm } = useResultContext()
+  const { results, isLoading, getResults, searchTerm } = useResultContext()
 
   const location = useLocation() // images, news, videos
 
-  // if (isLoading) {
-  //   return <Loading />
-  // }
-  return (
-    <div>Results</div>
-  )
+  if (isLoading) {
+    return <Loading />
+  }
+
+  console.log(location.pathname)
+
+  switch (location.pathname) {
+    case '/search':
+      return 'SEARCH'
+
+    default:
+      return 'ERROR!'
+  }
+
+
 }
 
 export default Results

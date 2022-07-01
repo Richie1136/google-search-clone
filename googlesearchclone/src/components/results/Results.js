631c24264b7171dd89c8e871dc029f8bdc4a96d1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-01 10:50:57 -0400

    Fixed an error inside of my ContextProvider that was causing my api key to not work

--- googlesearchclone/src/components/results/Results.js
+++ googlesearchclone/src/components/results/Results.js
@@ -12,13 +12,34 @@ const Results = () => {
 
   const location = useLocation() // images, news, videos
 
+  useEffect(() => {
+    getResults('/search/q=elon+musk')
+  }, [])
+
   if (isLoading) {
     return <Loading />
   }
 
+
+
   switch (location.pathname) {
     case '/search':
-      return 'SEARCH'
+      return (
+        <div className='flex flex-wrap justify-between space-y-6 sm:px-56'>
+          {results?.results?.map(({ link, title }, index) => (
+            <div key={index} className='md:w-2/5 w-full'>
+              <a href={link} target="_blank" rel='noreferrer'>
+                <p className='text-sm'>
+                  {link.length > 30 ? link.substring(0, 30) : link}
+                </p>
+                <p className='text-lg hover:underline dark:text-blue-300 text-blue-700'>
+                  {title}
+                </p>
+              </a>
+            </div>
+          ))}
+        </div>
+      )
     case '/images':
       return 'SEARCH'
     case '/news':
--- googlesearchclone/src/context/ResultContextProvider.js
+++ googlesearchclone/src/context/ResultContextProvider.js
@@ -13,18 +13,17 @@ export const ResultContextProvider = ({ children }) => {
 
 
   // /videos, /search, /images
-  const getResults = async (type) => {
+  const getResults = async (url) => {
     setIsLoading(true)
-    const response = await fetch(`${baseUrl}${type}`, {
+    const response = await fetch(`${baseUrl}${url}`, {
       method: 'GET',
       headers: {
-        headers: {
-          'X-RapidAPI-Key': 'e9f136fd86msh20374d953bfb05ep137d7ejsnaede58975cae',
-          'X-RapidAPI-Host': 'google-search3.p.rapidapi.com'
-        }
+        'X-RapidAPI-Key': 'e9f136fd86msh20374d953bfb05ep137d7ejsnaede58975cae',
+        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
       }
     })
     const data = await response.json()
+    console.log(data)
     setResults(data)
     setIsLoading(false)
   }
@@ -38,3 +37,40 @@ export const ResultContextProvider = ({ children }) => {
 }
 
 export const useResultContext = () => useContext(ResultContext)
+
+
+// import React, { createContext, useContext, useState } from 'react';
+
+// const StateContext = createContext();
+// const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+// export const StateContextProvider = ({ children }) => {
+//   const [results, setResults] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const getResults = async (url) => {
+//     setLoading(true);
+
+//     const res = await fetch(`${baseUrl}${url}`, {
+//       method: 'GET',
+//       headers: {
+//         'X-RapidAPI-Key': 'e9f136fd86msh20374d953bfb05ep137d7ejsnaede58975cae',
+//         'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+//       },
+//     });
+
+//     const data = await res.json();
+
+//     setResults(data);
+//     setLoading(false);
+//   };
+
+//   return (
+//     <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+//       {children}
+//     </StateContext.Provider>
+//   );
+// };
+
+// export const useStateContext = () => useContext(StateContext);

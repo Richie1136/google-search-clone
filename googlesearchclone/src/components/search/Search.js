540554c8a2b360cb11514fea8ed96085f9b762cb
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-08 10:51:05 -0400

    Added styling

--- googlesearchclone/src/components/search/Search.js
+++ googlesearchclone/src/components/search/Search.js
@@ -12,9 +12,22 @@ import { useResultContext } from "../../context/ResultContextProvider"
 // superman => 300, superman
 
 const Search = () => {
+  const [text, setText] = useState('')
+
+  const { setSearchTerm } = useResultContext()
+
+  const [debouncedValue] = useDebounce(text, 300)
+
+  useEffect(() => {
+    if (debouncedValue) {
+      setSearchTerm(debouncedValue)
+    }
+  }, [debouncedValue])
+
+
   return (
-    <div>
-      Search
+    <div className="relative sm: ml-48 md: ml-72 sm:mt-10 mt-3">
+      <input className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg" type='text' value={text} />
       <Links />
     </div>
   )

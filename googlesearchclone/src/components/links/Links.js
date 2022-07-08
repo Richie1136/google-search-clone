bee27f3015a88c1e436c8093afd1c30643782cd4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-08 10:43:03 -0400

    Importing packages into my Search component

--- googlesearchclone/src/components/links/Links.js
+++ googlesearchclone/src/components/links/Links.js
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom"
 
 const links = [
-  { url: '/search', text: 'All' },
-  { url: '/news', text: 'News' },
-  { url: '/image', text: 'Images' },
-  { url: '/video', text: 'Video' }
+  { url: '/search', text: '🔎  All' },
+  { url: '/news', text: '📰  News' },
+  { url: '/image', text: '📸 Images' },
+  { url: '/video', text: '📺 Videos' }
 
 ]
 
@@ -12,8 +12,11 @@ const Links = () => {
   return (
     <div className="flex sm:justify-around justify-between items-center mt-4">
       {links.map(({ url, text }) => (
-        <NavLink to={url} activeClassName='text-blue-500 border-b-2 dark:text-blue-300 border-blue-700 pb-2' />
-      ))}
+        <NavLink className='m-2 mb-0 flex justify-center' to={url} activeclassname='text-blue-500 border-b-2 dark:text-blue-300 border-blue-700 pb-2' >
+          {text}
+        </NavLink>
+      ))
+      }
     </div>
   )
 }
--- googlesearchclone/src/components/search/Search.js
+++ googlesearchclone/src/components/search/Search.js
@@ -1,4 +1,15 @@
 import Links from "../links/Links"
+import { useState, useEffect } from "react"
+
+import { useDebounce } from "use-debounce"
+import { useResultContext } from "../../context/ResultContextProvider"
+
+// useDebounce hook allows you to debounce any fast changing value. 
+// The debounced value will only reflect the latest value when the useDebounce hook 
+// has not been called for the specified time period. When used in conjunction with useEffect, 
+// you can easily ensure that expensive operations like API calls are not executed too frequently.
+
+// superman => 300, superman
 
 const Search = () => {
   return (
